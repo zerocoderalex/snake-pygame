e2840cf31e10c4a3fbf0fc3aa5f126636e17c384
zerocoderalex <alex12449@mail.ru>
2024-12-16 23:12:05 +0500

    test

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-from snake_game.game import main
+def add(a, b):
+    return a + b
 
-if __name__ == "__main__":
-    main()
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def mulitiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a - b
+
---
+++ master.py
@@ -0,0 +1,5 @@
+from snake_game.game import main
+
+if __name__ == "__main__":
+    main()
+
