ec9fef3aaddc854d8eef8c59946b4c4c5d5ac35a
zerocoderalex <alex12449@mail.ru>
2024-12-17 00:33:25 +0500

    correct

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-def add(a, b):
-    return a + b
-
-
-def subtract(a, b):
-    return a - b
-
-
-def multiply(a, b):
-    return a * b
-
-
-def subtract(a, b):
-    return a - b
-
-
-def mulitiply(a, b):
-    return a * b
-
-
-def divide(a, b):
-    return a / b
+def reminder(a, b):
+   if b == 0:
+       raise ValueError('На ноль делить нельзя')
+   return a % b
 
--- test.py
+++ test.py
@@ -1,24 +1,13 @@
 import unittest
-from main import add, subtract, multiply, divide
+from main import reminder
 
-class TestMath(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2, 5), 7)
-       # self.assertEqual(add(3, 7), 9)
+class TestCheck(unittest.TestCase):
+    def test_reminder(self):
+        self.assertEqual(reminder(5, 2), 1)
+        self.assertEqual(reminder(20, 4), 0)
 
-    def test_subtract(self):
-        self.assertEqual(subtract(7, 4), 3)
-        #self.assertEqual(subtract(4, 2), 1)
-
-    def test_multiply(self):
-        #self.assertEqual(multiply(2, 5), 12)
-        self.assertEqual(multiply(3, 6), 18)
-
-    def test_divide(self):
-        self.assertEqual(divide(20, 5), 4)
-        #self.assertEqual(divide(20, 4), 5)
-
-
+    def test_divide_by_zero(self):
+        self.assertRaises(ValueError, reminder, 6, 0)
 
 if __name__ == '__main__':
     unittest.main()
