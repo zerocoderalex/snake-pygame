adf80299100ffd48b926e972d77d4dbfa958969b
zerocoderalex <alex12449@mail.ru>
2024-12-20 22:52:47 +0500

    mockertest

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-def count_vowels(s):
-    # Определяем набор гласных букв
-    vowels = 'aeiouAEIOU'
-    count = 0
+import requests
 
-    # Проходим по каждому символу в строке
-    for char in s:
-        if char in vowels:
-            count += 1
+def get_cat_image():
+    url = "https://api.thecatapi.com/v1/images/search"
+    response = requests.get(url)
 
-    return count
-
+    if response.status_code == 200:
+        data = response.json()
+        # Извлекаем URL изображения
+        cat_image_url = data[0]['url']
+        return cat_image_url
+    else:
+        print(f"Ошибка при получении изображения: {response.status_code}")
+        return None
 
 # Пример использования
-# input_string = input("Введите строку: ")
-# vowel_count = count_vowels(input_string)
-# print(f"Количество гласных в строке: {vowel_count}")
-
-
+if __name__ == "__main__":
+    cat_image = get_cat_image()
+    if cat_image:
+        print(f"Случайное изображение кошки: {cat_image}")
--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 import pytest
-from main import count_vowels
+from main import get_cat_image
 
-def test_count_vowels():
-    assert count_vowels("alex") == 2
-    assert count_vowels("mkrt") == 0
-    assert count_vowels("alex24") == 2
+def test_get_cat_image_success(mocker):
+    mock_get = mocker.patch('main.requests.get')
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json.return_value = [{'url': 'https://example.com/cat.jpg'}]
 
-# Проверьте, что функция правильно считает гласные в строке, содержащей только гласные.
-# Проверьте, что функция возвращает 0 для строки, не содержащей гласных.
-# Проверьте, что функция правильно считает гласные в смешанных строках (включая прописные и строчные буквы).
+    cat_image_data = get_cat_image()
+    assert cat_image_data == 'https://example.com/cat.jpg'
+
+def test_get_cat_image_error(mocker):
+    mock_get = mocker.patch('main.requests.get')
+    mock_get.return_value.status_code = 404
+    mock_get.return_value.json.return_value = [{'url': 'https://example.com/cat.jpg'}]
+
+    cat_image_data = get_cat_image()
+    assert cat_image_data == None
+
+
